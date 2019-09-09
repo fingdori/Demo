88b8dc7b10e3299f5adb5916eb482db286a4e3a7
sanghyunbak <sanghyunbak@gmail.com>
2019-09-09 21:12:02 +0900

    add k8s test

---
+++ src/main/java/com/example/demo/k8s/Client.java
@@ -0,0 +1,54 @@
+package com.example.demo.k8s;
+
+import io.kubernetes.client.ApiClient;
+import io.kubernetes.client.ApiException;
+import io.kubernetes.client.Configuration;
+import io.kubernetes.client.apis.CoreV1Api;
+import io.kubernetes.client.models.V1Pod;
+import io.kubernetes.client.models.V1PodList;
+import io.kubernetes.client.util.ClientBuilder;
+import io.kubernetes.client.util.KubeConfig;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Client {
+    public static void k8sTest() throws IOException, ApiException {
+        ApiClient client = ClientBuilder.cluster().build();
+
+        // set the global default api-client to the in-cluster one from above
+        Configuration.setDefaultApiClient(client);
+
+        // the CoreV1Api loads default api-client from global configuration.
+        CoreV1Api api = new CoreV1Api();
+
+        // invokes the CoreV1Api client
+        V1PodList list;
+        list = api.listPodForAllNamespaces(null, null, null, null, null, null, null, null, false);
+        for (V1Pod item : list.getItems()) {
+            System.out.println(item.getMetadata().getName());
+        }
+    }
+
+    public static void runWithKubeConfig() throws IOException, ApiException {
+        // file path to your KubeConfig
+        String kubeConfigPath = "/Users/sanghyunbak/.kube/config";
+
+        // loading the out-of-cluster config, a kubeconfig from file-system
+        ApiClient client =
+                ClientBuilder.kubeconfig(KubeConfig.loadKubeConfig(new FileReader(kubeConfigPath))).build();
+
+        // set the global default api-client to the in-cluster one from above
+        Configuration.setDefaultApiClient(client);
+
+        // the CoreV1Api loads default api-client from global configuration.
+        CoreV1Api api = new CoreV1Api();
+
+        // invokes the CoreV1Api client
+        V1PodList list = api.listPodForAllNamespaces(null, null, null, null, null, null, null, null, true);
+        for (V1Pod item : list.getItems()) {
+            System.out.println(item.getMetadata().getName());
+        }
+    }
+}
---
+++ src/test/java/com/example/demo/k8s/ClientTest.java
@@ -0,0 +1,27 @@
+package com.example.demo.k8s;
+
+import io.kubernetes.client.ApiException;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static com.example.demo.k8s.Client.k8sTest;
+import static org.junit.Assert.*;
+
+public class ClientTest {
+    @Test
+    public void testK8s() {
+        try {
+            k8sTest();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void runWithKubeConfig() throws IOException, ApiException {
+        Client.runWithKubeConfig();
+    }
+}
